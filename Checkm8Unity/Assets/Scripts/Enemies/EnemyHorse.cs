25d6badf4b55c3887e8e79baba981612ff8bfc6f
StefandePutter <Stefandeputter@gmail.com>
2025-04-02 12:44:43 +0200

    Update EnemyHorse.cs

--- Checkm8Unity/Assets/Scripts/Enemies/EnemyHorse.cs
+++ Checkm8Unity/Assets/Scripts/Enemies/EnemyHorse.cs
@@ -6,12 +6,6 @@ public class EnemyHorse : EnemyBase
     [SerializeField] private float _rotateSpeed;
     private Transform _playerTransform;
 
-    protected override void Start()
-    {
-        base.Start();
-        //_playerTransform = GameManager.s_Player.transform;
-    }
-
     protected override void Shoot()
     {
         return;
@@ -23,28 +17,19 @@ public class EnemyHorse : EnemyBase
         bool hitPlayer = false;
 
         RaycastHit hit;
-        //if (Physics.Raycast(transform.position + Vector3.up * 0.5f, transform.TransformDirection(target.position), out hit, Mathf.Infinity, LayerMask.GetMask("Player", "Enemy", "Environment"), QueryTriggerInteraction.Ignore))
-        //{
-        //    // Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * hit.distance, Color.green);
-        //    Debug.DrawRay(transform.position + Vector3.up * 0.5f, transform.TransformDirection(target.position) * hit.distance, Color.green);
-        //    Debug.Log("hit " + hit.transform.tag + " " + hit.distance);
-        //    hitPlayer = true;
-        //} else
-        //{
-        //    // Debug.DrawRay(transform.position, target.position * 100, Color.green);
-        //    Debug.DrawRay(transform.position + Vector3.up * 0.5f, transform.TransformDirection(target.position) * 100, Color.green);
-
-        //}
         if (Physics.Raycast(transform.position + Vector3.up * 0.5f, ((target.position + Vector3.up * 0.5f) - (transform.position + Vector3.up * 0.5f)), out hit, Mathf.Infinity, LayerMask.GetMask("Player", "Enemy", "Environment"), QueryTriggerInteraction.Ignore))
         {
-            Debug.Log("hit " + hit.transform.name);
             if (hit.transform == target)
             {
                 // In Range and i can see you!
                 Debug.Log("hit player");
                 // Charge();
+                StartCoroutine(Charge());
                 
                 hitPlayer = true;
+            } else
+            {
+                hitPlayer = false;
             }
         }
 
