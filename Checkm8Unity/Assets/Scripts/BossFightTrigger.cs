891ca4f1b06010bc26fad2d18e36f6a986e2bfbb
StefandePutter <Stefandeputter@gmail.com>
2025-04-04 15:58:47 +0200

    Update BossFightTrigger.cs

--- Checkm8Unity/Assets/Scripts/BossFightTrigger.cs
+++ Checkm8Unity/Assets/Scripts/BossFightTrigger.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class BossFightTrigger : MonoBehaviour
 {
     private bool _isTriggered;
+    private GameManager _gameManager;
+    private Camera _camera;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _gameManager = GameManager.s_Instance;
+        _camera = Camera.main;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log(other.CompareTag("Player"));
     }
 }
